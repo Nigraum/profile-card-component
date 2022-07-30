8f823a06c2cae700b1876c87665495368d77d61f
Nigraum <yan.corintima@hotmail.com>
2022-07-30 19:49:04 -0300

    Config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'md': '768px',
+      },
+      fontFamily: {
+        kumbh: ['Kumbh Sans', 'sans-serif'],
+      },
+      colors: {
+        darkCyan: {
+          300: 'hsl(185, 75%, 39%)',
+        },
+        desaturated: {
+          300: 'hsl(229, 23%, 23%)',
+        },
+        grayBlue: {
+          300: 'hsl(227, 10%, 46%)',
+        },
+        darkGray: {
+          300: 'hsl(0, 0%, 59%)',
+        }
+      }
+    },
   },
   plugins: [],
 }
